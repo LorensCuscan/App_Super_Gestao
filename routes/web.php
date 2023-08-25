14c9ab9409d745e0810c879fcf8ceceb0b2a6413
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-08-25 16:28:33 -0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\TesteController;
 use Illuminate\Support\Facades\Route;
 use PhpParser\Node\Stmt\Static_;
 
@@ -51,18 +52,12 @@ Route::prefix('/app')->group(function(){
 });
 
 
-Route::get('/rota1', function(){
-    echo 'Rota 1';
-})->name('site.rota1');
-
-
-Route::get('/rota2', function(){
- return redirect()->route('site.rota1');
-})->name('site.rota2');
+Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste');
 
 
 Route::fallback(function(){
-    echo 'A rota acessada não existe!. <a href="' .route('site.index'). '">clique aqui para voltar para pagina inicial</a>';
+   echo 'A rota acessada não existe!. <a href="' .route('site.index'). '">clique aqui para voltar para pagina inicial</a>';
+    
 });
 
 //Route::redirect('/rota2', '/rota1');
