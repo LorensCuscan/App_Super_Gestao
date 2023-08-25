03d187a2d6d43321c10c1c135077c357e65db312
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-08-25 14:37:20 -0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -30,13 +30,11 @@ Route::get('/contato', [ContatoController::class, 'contato']);
 
 
 Route::get(
-    '/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}',
+    '/contato/{nome}/{categoria_id}',
  function(
-    $nome      = 'Mensagem não informada',
-    $categoria = 'Informação',
-    $assunto   = 'Contato',
-    $mensagem  = 'Mensagem não informada',
+    $nome      = 'Desconhecido',
+    $categoria_id = 1
     ){
-        echo "Estamos aqui: " . $nome . " - " . $categoria . " - " . $assunto . " - " . $mensagem;
-});
+        echo "Estamos aqui: " . $nome . " - " . $categoria_id;
+})->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
 
