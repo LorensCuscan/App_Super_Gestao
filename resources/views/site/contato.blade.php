2396d237431da99e878a4784aa861d174dcda997
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-08-31 15:33:55 -0300

    fix

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -12,7 +12,7 @@
 
     <div class="informacao-pagina">
         <div class="contato-principal">
-           @component('site.layouts._components.form_contato', ['classe' => 'borda-preta'])
+           @component('site.layouts._components.form_contato', ['classe' => ''])
             <p>A nossa equipe analisará a sua mensagem e retornaremos o mais breve possivel!</p>
             <p>Nosso tempo médio de resposta é de 48 horas.</p>
            @endcomponent        
