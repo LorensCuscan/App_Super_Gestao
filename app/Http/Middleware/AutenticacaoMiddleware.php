11024e90abdb4ed8a30a9187408f44161a85d60e
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-09-02 03:07:41 -0300

    fix on middlewares

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -13,12 +13,25 @@ class AutenticacaoMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next,): Response
+    public function handle(Request $request, Closure $next, $metodo_autenticacao, $perfil): Response
     {
-
+        echo $metodo_autenticacao. '-' . $perfil .'<br>';
         
         //verifica se o usuario possui acesso a rota
-        if(true){
+        if($metodo_autenticacao == 'padrao'){
+            echo 'verificar usuario e senha no db'. $perfil .'<br>';
+        }
+        if($metodo_autenticacao == 'ldap'){
+            echo 'verificar usuario e senha no ad'. $perfil .'<br>';
+        }
+
+        if ($perfil == 'visitante'){
+            echo 'exibir apenas alguns recursos';
+        }else{
+            echo 'faz o login';
+        }
+
+        if(false){
             return $next($request);
 
             
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\FornecedorController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\TesteController;
@@ -36,14 +37,14 @@ Route::get('/contato', [ContatoController::class, 'contato'])
 
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
 
-Route::get('/login', function(){
-    return 'Login';
-})->name('site.login');
+
+Route::get('/login', [LoginController::class, 'index'])->name('site.login');
 
 
 
 
-Route::middleware('log.acesso')->prefix('/app')->group(function(){
+
+Route::middleware('log.autenticacao:padrao,visitante')->prefix('/app')->group(function(){
 
     Route::get('/clientes', function(){ return 'Clientes';})->name('app.clientes');
     Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');   
