cc28f0f61159cb11c59fc63127ac7e55ebf6203d
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-09-02 03:56:42 -0300

    creating login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,7 +10,21 @@ class LoginController extends Controller
         return view('site.login', ['titulo' => 'Login']);
     }
 
-    public function autenticar(){
-        return 'chegamos aqui';
+    //regras de validação
+    public function autenticar(Request $request){
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+        $feedback = [
+            'usuario.email' => 'O campo usúario (e-mail) é obrigatorio',
+            'senha.required' => 'O campo senha é obrigatorio'
+        ];
+        //se não passar pelo validate
+        $request->validate($regras, $feedback);
+        //as mensagens de feesback de validação
+
+
+        print_r($request->all());
     }
 }
