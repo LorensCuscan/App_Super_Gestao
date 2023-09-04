d0c0bc7c849bf43b90a655df49836c60d45c4b31
Lorens Cuscan Alcala <lorenscuscan@gmail.com>
2023-09-03 21:02:35 -0300

    Login and page for clients done

--- resources/views/app/layouts/_partials/topo.blade.php
+++ resources/views/app/layouts/_partials/topo.blade.php
@@ -7,9 +7,9 @@
     <div class="menu">
         <ul>
             <li><a href="{{ route('app.home') }}">Home</a></li>
-            <li><a href="{{ route('app.cliente') }}">Cliente</a></li>
-            <li><a href="{{ route('app.fornecedor') }}">Fornecedor</a></li>
-            <li><a href="{{ route('app.produto') }}">Produto</a></li>
+            <li><a href="{{ route('app.clientes') }}">Cliente</a></li>
+            <li><a href="{{ route('app.fornecedores') }}">Fornecedor</a></li>
+            <li><a href="{{ route('app.produtos') }}">Produto</a></li>
             <li><a href="{{ route('app.sair') }}">Sair</a></li>
         </ul>
     </div>
